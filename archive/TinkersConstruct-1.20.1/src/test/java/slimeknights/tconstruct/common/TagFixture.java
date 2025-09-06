576a5439e240efcb123cd9b4b64611603d3bf62b
siogu DKJ <15715022545@163.com>
2025-09-06 22:49:30 +0800

    ci: ignore archive/ directory

--- archive/TinkersConstruct-1.20.1/src/test/java/slimeknights/tconstruct/common/TagFixture.java
+++
@@ -1,8 +0,0 @@
-package slimeknights.tconstruct.common;
-
-/** Helper for all our tag needs, right now we just mark tags as loaded always as we don't use them in tests. */
-public class TagFixture {
-  public static void init() {
-    TinkerTags.tagsLoaded = true;
-  }
-}
